bae79a0b3aab5fa949274951fe2e3886b80b6f0d
CLAYJJ <1198201587@qq.com>
2018-06-11 21:42:13 +0800

    aop

--- src/main/java/com/sso/cas/server/aop/Example.java
+++ src/main/java/com/sso/cas/server/aop/Example.java
@@ -1,6 +1,7 @@
 package com.sso.cas.server.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,6 +10,21 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
+/*
+ *
+ *
+ * execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?name-pattern(param-pattern)
+ *          throws-pattern?)
+ *
+ * 执行结果：
+ * 方法：findAllaround 前！
+ * 方法：findAll执行前执行！
+ * 方法：findAllaround 后！
+ * 方法：findAll执行后执行！
+ *
+ * @Author luo jiajia
+ * @Date 2018/6/10 0010 下午 17:00
+ */
 
 @Aspect
 @Component
@@ -26,10 +42,13 @@ public class Example {
         System.out.println("方法："+method.getName()+"执行后执行！");
     }
 
-   /* @Around("execution(* com.sso.cas.server.service.*.*(..))")
-    public void around(JoinPoint joinPoint){
-        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+    @Around("execution(* com.sso.cas.server.service.*.*(..))")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
         Method method = signature.getMethod();
-        System.out.println("方法："+method.getName()+"执行前后执行！");
-    }*/
+        System.out.println("方法："+method.getName()+"around 前！");
+        Object retVal = proceedingJoinPoint.proceed();
+        System.out.println("方法："+method.getName()+"around 后！");
+        return retVal;
+    }
 }
